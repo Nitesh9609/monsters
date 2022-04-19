144032b27ec89bb187fd8318370dd0e3b46c9eb0
Nitesh9609 <sharmanitesh920@gmail.com>
2022-04-19 17:22:38 +0530

    commit5

---
+++ src/components/search/search.component.jsx
@@ -0,0 +1,18 @@
+import './search.style.css'
+const Search = (props) => {
+    return(
+
+        <>
+            <input className='search-box' 
+            type = "text" 
+            placeholder="Search Monsters"
+       />
+
+            
+        
+        </>
+       
+    )
+}
+
+export default Search
